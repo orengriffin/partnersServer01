eb56f53c25f836973e89bb0d226fad091ac85569
orengriffin <oren.griffin@gmail.com>
2015-01-11 16:53:46 +0200

    migration almost complete

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
     'mongodb://heroku_app31337616:obtp59dcp2qqaiushniu6ea4cu@ds049130.mongolab.com:49130/heroku_app31337616';
-//app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('port', process.env.PORT || 3010);
 
