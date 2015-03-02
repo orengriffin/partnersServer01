fb4d579521f77405c5cd364b8b4b3c57672acc7e
orengriffin <oren.griffin@gmail.com>
2015-03-02 19:29:26 +0200

    - force Push

--- sendMail.js
+++ sendMail.js
@@ -24,7 +24,6 @@ var sendMailFunctions = {
     validEmailAddress: function (email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
-
     },
 
 
